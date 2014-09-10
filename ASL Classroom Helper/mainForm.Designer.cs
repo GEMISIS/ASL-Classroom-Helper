1d875f624dc6c9623da88f22d04574ffd5714e14
Gerald McAlister <gerald.mcalister.gem@gmail.com>
2014-09-10 15:38:50 -0400

    Initial Commit
    -Draws the video frames received from a Microsoft Kinect.
    -Draws the recognized text to the screen.

---
+++ ASL Classroom Helper/mainForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ASL_Classroom_Helper
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.kinectPictureBox = new System.Windows.Forms.PictureBox();
+            this.kinectStatusLabel = new System.Windows.Forms.Label();
+            this.speechTextLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.kinectPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kinectPictureBox
+            // 
+            this.kinectPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.kinectPictureBox.Name = "kinectPictureBox";
+            this.kinectPictureBox.Size = new System.Drawing.Size(600, 391);
+            this.kinectPictureBox.TabIndex = 0;
+            this.kinectPictureBox.TabStop = false;
+            // 
+            // kinectStatusLabel
+            // 
+            this.kinectStatusLabel.AutoSize = true;
+            this.kinectStatusLabel.Location = new System.Drawing.Point(558, 419);
+            this.kinectStatusLabel.Name = "kinectStatusLabel";
+            this.kinectStatusLabel.Size = new System.Drawing.Size(54, 13);
+            this.kinectStatusLabel.TabIndex = 1;
+            this.kinectStatusLabel.Text = "No Kinect";
+            // 
+            // speechTextLabel
+            // 
+            this.speechTextLabel.AutoSize = true;
+            this.speechTextLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.speechTextLabel.Location = new System.Drawing.Point(7, 406);
+            this.speechTextLabel.Name = "speechTextLabel";
+            this.speechTextLabel.Size = new System.Drawing.Size(87, 26);
+            this.speechTextLabel.TabIndex = 2;
+            this.speechTextLabel.Text = "No Text";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.speechTextLabel);
+            this.Controls.Add(this.kinectStatusLabel);
+            this.Controls.Add(this.kinectPictureBox);
+            this.Name = "mainForm";
+            this.Text = "ASL Classroom Helper";
+            ((System.ComponentModel.ISupportInitialize)(this.kinectPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox kinectPictureBox;
+        private System.Windows.Forms.Label kinectStatusLabel;
+        private System.Windows.Forms.Label speechTextLabel;
+    }
+}
+
---
+++ ASL Classroom Helper/mainForm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Kinect;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Speech.Recognition;
+using System.Speech.AudioFormat;
+
+namespace ASL_Classroom_Helper
+{
+    public partial class mainForm : Form
+    {
+        private KinectSensor kinect;
+        private byte[] colorPixels;
+        private Bitmap colorBitmap;
+        private Rectangle imageRectangle;
+
+        private SpeechRecognitionEngine speechEngine;
+        private DictationGrammar dictationGrammar;
+
+        public bool setupKinect()
+        {
+            if(KinectSensor.KinectSensors.Count > 0)
+            {
+                this.kinect = KinectSensor.KinectSensors[0];
+
+                this.kinect.ColorFrameReady += kinect_ColorFrameReady;
+                this.kinect.ColorStream.Enable();
+                this.kinect.Start();
+
+                this.colorPixels = new byte[this.kinect.ColorStream.FramePixelDataLength];
+                this.imageRectangle = new Rectangle(0, 0, this.kinect.ColorStream.FrameWidth, this.kinect.ColorStream.FrameHeight);
+                this.colorBitmap = new Bitmap(this.kinect.ColorStream.FrameWidth, this.kinect.ColorStream.FrameHeight, PixelFormat.Format32bppRgb);
+
+                this.dictationGrammar = new DictationGrammar();
+                this.dictationGrammar.Name = "default dication";
+                this.dictationGrammar.Enabled = true;
+
+                this.speechEngine = new SpeechRecognitionEngine();
+                this.speechEngine.LoadGrammar(this.dictationGrammar);
+                this.speechEngine.SpeechHypothesized += speechEngine_SpeechHypothesized;
+                this.speechEngine.SpeechRecognized += speechEngine_SpeechRecognized;
+                this.speechEngine.SetInputToAudioStream(
+                                    this.kinect.AudioSource.Start(), 
+                                    new SpeechAudioFormatInfo(EncodingFormat.Pcm, 16000, 16, 1, 32000, 2, null));
+                this.speechEngine.RecognizeAsync(RecognizeMode.Multiple);
+
+                return true;
+            }
+            return false;
+        }
+
+        void speechEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            this.speechTextLabel.Text = e.Result.Text;
+        }
+
+        void speechEngine_SpeechHypothesized(object sender, SpeechHypothesizedEventArgs e)
+        {
+        }
+
+        IntPtr ptr;
+        BitmapData bmpData;
+
+        void kinect_ColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+            {
+                if (colorFrame != null)
+                {
+                    colorFrame.CopyPixelDataTo(this.colorPixels);
+                    bmpData = colorBitmap.LockBits(this.imageRectangle, ImageLockMode.WriteOnly, colorBitmap.PixelFormat);
+                    ptr = bmpData.Scan0;
+                    Marshal.Copy(this.colorPixels, 0, ptr, colorFrame.PixelDataLength);
+                    this.colorBitmap.UnlockBits(bmpData);
+                    this.kinectPictureBox.Image = this.colorBitmap;
+                }
+            }
+        }
+
+        public mainForm()
+        {
+            InitializeComponent();
+
+            if(this.setupKinect())
+            {
+                this.kinectStatusLabel.Text = "Kinected!";
+            }
+        }
+    }
+}
